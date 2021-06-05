fa870342f325bad9131a1e36becfa8cf8ffe3ec5
edubernardi <eduardobernardi@outlook.com.br>
2021-06-04 22:14:33 -0300

    Writing and loading memory into files

--- src/com/company/UC.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public class UC {
-    //responsavel pelas operacoes que devem ser realizadas e quando devem ser realizadas (controla ula e udi)
-}
--- src/com/company/UDI.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public class UDI {
-    //recebe o codigo (OPCODE) da operacao a ser executada e decodifica, manda a UC sobre a instrucao (leitura de dados)
-}
