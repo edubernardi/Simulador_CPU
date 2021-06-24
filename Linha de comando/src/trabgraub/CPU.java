bdedbda25115d90757b6abe1b0e7316ec2b696ab
edubernardi <77065000+edubernardi@users.noreply.github.com>
2021-06-23 21:27:16 -0300

    Add files via upload

---
+++ Linha de comando/src/trabgraub/CPU.java
@@ -0,0 +1,13 @@
+package trabgraub;
+
+public class CPU {
+    private UC uc;
+
+    public CPU() {
+        uc = new UC();
+    }
+
+    public void rodar() {
+        uc.rodar();
+    }
+}
---
+++ Linha de comando/src/trabgraub/ULA.java
@@ -0,0 +1,66 @@
+package trabgraub;
+
+public class ULA {
+    private Registrador A;
+    private Registrador B;
+    private Registrador C;
+
+
+    public ULA() {
+        A = new Registrador();
+        B = new Registrador();
+        C = new Registrador();
+    }
+
+    public void sum() {
+        int resultado = A.getValor() + B.getValor();
+        if (resultado > 255) {
+            resultado -= 255;
+        }
+        C.setValor(resultado);
+    }
+
+    public void sub() {
+        int resultado = A.getValor() - B.getValor();
+        if (resultado < 0) {
+            resultado += 255;
+        }
+        C.setValor(resultado);
+    }
+
+    public void comp() {
+        int a = A.getValor();
+        int b = B.getValor();
+        if (a == b) {
+            C.setValor(0);
+        } else if (a < b) {
+            C.setValor(1);
+        } else {
+            C.setValor(2);
+        }
+    }
+
+    public Registrador getA() {
+        return A;
+    }
+
+    public void setA(int a) {
+        A.setValor(a);
+    }
+
+    public Registrador getB() {
+        return B;
+    }
+
+    public void setB(int b) {
+        B.setValor(b);
+    }
+
+    public Registrador getC() {
+        return C;
+    }
+
+    public void setC(int c) {
+        C.setValor(c);
+    }
+}
